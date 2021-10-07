28a89afdc458b178e653a2a562f70e2f7c45b449
Guilherme Colhado <guic170602@gmail.com>
2021-10-07 18:46:00 -0300

    Respondendo as funcoes em js

--- src/katas1.js
+++ src/katas1.js
@@ -1,94 +1,101 @@
 function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+  let i=1
+  let s=[]  
+  while (i<=20) {
+    s.push(i)
+    i++
+  }   
+  return s
 }
-
-//call function oneThroughTwenty
+console.log(`A sequencia de numeros é: ${oneThroughTwenty()}`)
 
 function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+  let x = []
+  for (let i = 2; i <= 20; i+=2) {
+    x.push(i)    
+  }
+  return x
 }
+console.log(`A sequencia de numeros pares é: ${evensToTwenty()}`)
 
-//call function evensToTwenty
-
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+function oddsToTwenty(){
+  let x = []
+  for (let i = 1; i <= 20; i+=2) {
+    x.push(i)    
+  }
+  return x
 }
-
-//call function oddsToTwenty
+console.log(`A sequencia de numeros impares é: ${oddsToTwenty()}`)
 
 function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-
+  let x = []
+  for (let i = 5; i <= 100; i+=5) {
+    x.push(i)    
+  }
+  return x
 }
-
-//call function multiplesOfFive
+console.log(`A sequencia de numeros multiplos de 5 é: ${multiplesOfFive()}`)
 
 function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+  let i = 1
+  let quadrado = 0
+  let x = []
+  while (quadrado < 100) {
+    quadrado = i**2
+    x.push(quadrado)
+    i++
+  }
+  return x
 }
-
-//call function squareNumbers
+console.log(`A sequencia de numeros quadrados perfeitos é: ${squareNumbers()}`)
 
 function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let i=20
+  let s=[]  
+  while (i>=1) {
+    s.push(i)
+    i--
+  }   
+  return s
 }
-
-//call function countingBackwards
+console.log(`A sequencia de numeros de tras pra frente é: ${countingBackwards()}`)
 
 function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let x = []
+  for (let i = 20; i >= 2; i-=2) {
+    x.push(i)    
+  }
+  return x
 }
-
-//call function evenNumbersBackwards
+console.log(`A sequencia de numeros pares de tras pra frente é: ${evenNumbersBackwards()}`)
 
 function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let x = []
+  for (let i = 19; i >= 1; i-=2) {
+    x.push(i)    
+  }
+  return x
 }
-
-//call function oddNumbersBackwards
+console.log(`A sequencia de numeros impares de tras pra frente é: ${oddNumbersBackwards()}`)
 
 function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let x = []
+  for (let i = 100; i >= 5; i-=5) {
+    x.push(i)    
+  }
+  return x
 }
+console.log(`A sequencia de numeros multiplos de 5 de tras pra frente é: ${multiplesOfFiveBackwards()}`)
 
-//call function multiplesOfFiveBackwards
-
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function squareNumbersBackwards(){
+  let i = 10
+  let quadrado = 100
+  let x = []
+  while (quadrado > 1) {
+    quadrado = i**2
+    x.push(quadrado)
+    i--
+  }
+  return x
 }
-
-//call function squareNumbersBackwards
+console.log(`A sequencia de numeros quadrados perfeitos de tras pra frente é: ${squareNumbersBackwards()}`)
